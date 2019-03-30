09dac2b6530d08e8fccadd745b89b6c8d40e412b
AndreBurkett <andre.burkett@gmail.com>
2019-03-30 01:45:39 -0700

    log

--- KCODE3/sectorController.js
+++ KCODE3/sectorController.js
@@ -8,7 +8,7 @@ exports.sectorController = class{
             let source = this.room.find(FIND_SOURCES);
             console.log(source);
             for(let i in source){
-                console.log(source[i]);
+                console.log(source[i].id);
                 Memory.sector[this.room.name].source.push(source[i]);
                 Memory.sector[this.room.name].source[i].space = this.getfreeSpace(Game.getObjectById(source[i]));
             }
