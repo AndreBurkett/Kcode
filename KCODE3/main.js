fb6cacf9f5138a8ec8e8969f24026d0cddae3c9d
AndreBurkett <andre.burkett@gmail.com>
2019-03-30 15:59:09 -0700

    bug

--- KCODE3/main.js
+++ KCODE3/main.js
@@ -5,13 +5,15 @@ module.exports.loop = function(){
     for(var name in Memory.creeps){
         if(!Game.creeps[name]){
             let assignment = Memory.creeps[name].assignment;
-            switch(Memory.creeps[name].role){
-                case 'miner':
-                    delete Memory.source[assignment].miner[Memory.creeps[name].id];
-                    break;
-                case 'upgrader':
-                    delete Memory.controller[assignment].upgrader[Memory.creeps[name].id];
-                    break;
+            if(assignment){
+                switch(Memory.creeps[name].role){
+                    case 'miner':
+                        delete Memory.source[assignment].miner[Memory.creeps[name].id];
+                        break;
+                    case 'upgrader':
+                        delete Memory.controller[assignment].upgrader[Memory.creeps[name].id];
+                        break;
+                }
             }
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
