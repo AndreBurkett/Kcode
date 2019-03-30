70b66571b308dc9e397ea45bea2c6e901831a48b
AndreBurkett <andre.burkett@gmail.com>
2019-03-30 13:55:28 -0700

    del mem

--- KCODE3/main.js
+++ KCODE3/main.js
@@ -4,6 +4,13 @@ module.exports.loop = function(){
     //Clear Memory
     for(var name in Memory.creeps){
         if(!Game.creeps[name]){
+            let assignment = Memory.creeps[name].assignment;
+            switch(Memory.creeps[name].role){
+                case 'miner':
+                    delete Memory.source[assignment].miner[Game.creeps[name]];
+                case 'upgrader':
+                    delete Memory.controller[assignment].upgrader[Game.creeps[name]];
+            }
             delete Memory.creeps[name];
             console.log('Clearing non-existing creep memory:', name);
         }
