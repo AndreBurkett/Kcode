07b1b1e8075bf0d171e989bb189f3e5872f2e595
AndreBurkett <andre.burkett@gmail.com>
2019-03-30 02:06:39 -0700

    log

--- KCODE3/sectorController.js
+++ KCODE3/sectorController.js
@@ -19,7 +19,7 @@ exports.sectorController = class{
             for(let i=-1;i<=1;i++){
                 for(let j=-1;j<=1;j++){
                     if(this.terrain.get(pos.x + i, pos.y + j) != 1){
-                        console.log(this.terrain.get(pos.x + i, pos.y + j));
+                        console.log(pos.x + i, pos.y + j);
                         space++;
                     }
                 }
