83afe5e1ad70ea80f14b45ab6d35a73ed2eed92b
AndreBurkett <andre.burkett@gmail.com>
2019-04-01 00:44:35 -0700

    debug

--- KCODE3/role.miner.js
+++ KCODE3/role.miner.js
@@ -4,15 +4,12 @@ var miner = {
         if(creep.carry.energy == creep.carryCapacity){
             let pos = new RoomPosition(Memory.source[source].spawnPath.path[0].x, Memory.source[source].spawnPath.path[0].y, Memory.source[source].spawnPath.path[0].roomName);
             let container = pos.lookFor(STRUCTURE_CONTAINER)[0];
-            let site = pos.findClosestByRange(FIND_CONSTRUCTION_SITES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER});
-            console.log(site);
-            switch(creep.transfer(container, RESOURCE_ENERGY)){
-                case ERR_NOT_IN_RANGE:
-                    creep.moveTo(container);
-                    break;
-                case ERR_INVALID_TARGET:
-                    creep.build(container);
-                    break;
+            if(container){
+                if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) creep.moveTo(container);
+            }
+            else{
+                let site = pos.findClosestByRange(FIND_CONSTRUCTION_SITES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER});
+                if(creep.build(container) == ERR_NOT_IN_RANGE) creep.moveTo(site);
             }
         }
         else{
