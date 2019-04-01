4c693a0a0f135a4aa96d3efb240892c8cbe2d1ae
AndreBurkett <andre.burkett@gmail.com>
2019-04-01 13:54:05 -0700

    builder

--- KCODE3/taskManager.js
+++ KCODE3/taskManager.js
@@ -1,3 +1,4 @@
+var builder = require('role.builder');
 var miner = require('role.miner');
 var transporter = require('role.transporter');
 var upgrader = require('role.upgrader');
@@ -5,6 +6,9 @@ var upgrader = require('role.upgrader');
 exports.creepManager = class{
     constructor(creep){
         switch(creep.memory.role){
+            case 'builder':
+                builder.run(creep);
+                break;
             case 'miner':
                 miner.run(creep);
                 break;
