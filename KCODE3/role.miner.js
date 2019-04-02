d391895cf60ba66bb7df594d30949b8a833fa882
AndreBurkett <andre.burkett@gmail.com>
2019-04-02 07:27:48 -0700

    bug fix

--- KCODE3/role.miner.js
+++ KCODE3/role.miner.js
@@ -3,7 +3,7 @@ var miner = {
         let source = creep.memory.assignment;
         if(creep.carry.energy == creep.carryCapacity){
             let pos = new RoomPosition(Memory.source[source].spawnPath.path[0].x, Memory.source[source].spawnPath.path[0].y, Memory.source[source].spawnPath.path[0].roomName);
-            let container = pos.lookFor(STRUCTURE_CONTAINER)[0];
+            let container = pos.lookFor(LOOK_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER})[0];
             if(container){
                 if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) creep.moveTo(container);
             }
