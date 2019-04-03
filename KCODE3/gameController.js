d367b0dbe6d16acf25a25f9840b68a3080f4f1cc
AndreBurkett <andre.burkett@gmail.com>
2019-04-03 15:34:57 -0700

    debug

--- KCODE3/gameController.js
+++ KCODE3/gameController.js
@@ -74,8 +74,8 @@ exports.gameController = class{
                         let creep = Game.getObjectById(Memory.source[i].miner[j]);
                         if(creep){
                             workParts += _.filter(creep.body, function(bp){return bp == WORK;}).length;
-                            console.log(Memory.source[i].miner[j].length);
-                            if(workParts < 5 && Memory.source[i].miner[j].length < Memory.source[i].space){
+                            console.log(Memory.source[i].miner.length);
+                            if(workParts < 5 && Memory.source[i].miner.length < Memory.source[i].space){
                                 this.assigner.assignMiner(5-workParts, i);
                             }
                         }
