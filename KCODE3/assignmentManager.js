6279ef4f787ffca3cc8a3e143141c5059f140b36
AndreBurkett <andre.burkett@gmail.com>
2019-04-03 13:52:00 -0700

    debug

--- KCODE3/assignmentManager.js
+++ KCODE3/assignmentManager.js
@@ -11,6 +11,7 @@ exports.assignmentManager = class{
     }
 
     assignBuilder(site, type){
+        Memory.controller[type][site].builder.push('test');
         if(this.builder.length > 0 ){
             for(let i in this.builder){
                 Memory.construction[type][site].builder.push(this.builder[i].id);
