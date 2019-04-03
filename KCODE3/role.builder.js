35b737013fef0209f90fe6ad0f0195d2378757cb
AndreBurkett <andre.burkett@gmail.com>
2019-04-03 11:41:02 -0700

    slight bug fix

--- KCODE3/role.builder.js
+++ KCODE3/role.builder.js
@@ -3,10 +3,12 @@ var builder = {
         var target;
         if(creep.carry.energy == creep.carryCapacity || (creep.carry.energy > 0 && creep.memory.task == 'build')){
             creep.memory.task = 'build';
-            console.log(Game.constructionSites[creep.memory.assignment]);
-            target = Game.getObjectById(creep.memory.assignment);
+            target = Game.constructionSites[creep.memory.assignment];
             if(target){
                 if(creep.build(target) == ERR_NOT_IN_RANGE) creep.moveTo(target);
+            }
+            else{
+                delete creep.memory.assignment;
             }
         }
         else if(creep.room.find(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0}).length > 0){
