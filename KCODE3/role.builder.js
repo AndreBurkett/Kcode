5a3e301848130fd699a710059b1e6044827ce019
AndreBurkett <andre.burkett@gmail.com>
2019-04-03 11:39:16 -0700

    log

--- KCODE3/role.builder.js
+++ KCODE3/role.builder.js
@@ -3,6 +3,7 @@ var builder = {
         var target;
         if(creep.carry.energy == creep.carryCapacity || (creep.carry.energy > 0 && creep.memory.task == 'build')){
             creep.memory.task = 'build';
+            console.log(Game.constructionSites[creep.memory.assignment]);
             target = Game.getObjectById(creep.memory.assignment);
             if(target){
                 if(creep.build(target) == ERR_NOT_IN_RANGE) creep.moveTo(target);
