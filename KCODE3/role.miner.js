116b1b0eb3468ca5ff1d1cda09a2273c228e41c9
AndreBurkett <andre.burkett@gmail.com>
2019-04-04 23:29:39 -0700

    transport if needed

--- KCODE3/role.miner.js
+++ KCODE3/role.miner.js
@@ -1,18 +1,36 @@
 var miner = {
     run: function (creep) {
-        let source = creep.memory.assignment;
-        if(creep.carry.energy == creep.carryCapacity){
-            console.log(Memory.source[source].transporter.length);
+        function transport(){
+            let target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: (s) => (s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity) || ((s.structureType == STRUCTURE_EXTENSION || s.structureType == STRUCTURE_SPAWN) && s.energy < s.energyCapacity)
+            });
+            if (target){
+                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+                }
+            }
+            else dump();
+        }
+
+        function dump(){
             let pos = new RoomPosition(Memory.source[source].spawnPath.path[0].x, Memory.source[source].spawnPath.path[0].y, Memory.source[source].spawnPath.path[0].roomName);
             let container = pos.lookFor(LOOK_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER})[0];
             if(container){
                 if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) creep.moveTo(container);
             }
             else{
-                //let site = pos.findClosestByRange(FIND_CONSTRUCTION_SITES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER});
-                //if(creep.build(site) == ERR_NOT_IN_RANGE) creep.moveTo(site);
                 let target = Game.getObjectById(source);
                 if(creep.harvest(target) == ERR_NOT_IN_RANGE) creep.moveTo(target);
+            }
+        }
+        
+        let source = creep.memory.assignment;
+        if(creep.carry.energy == creep.carryCapacity){
+            if(Memory.source[source].transporter.length == 0){
+                transport();
+            }
+            else{
+                dump();
             }
         }
         else{
