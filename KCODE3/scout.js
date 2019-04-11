4715dc22a53ccbba5d9f14d958dc4b181dc60f1c
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 00:23:01 -0700

    delog

--- KCODE3/scout.js
+++ KCODE3/scout.js
@@ -2,7 +2,6 @@ exports.scout = class{
     constructor(creep){
         this.creep = creep;
         this.assignment = creep.memory.assignment;
-        console.log(this.assignment);
         if(this.assignment){
             let pos = new RoomPosition(25,25,this.assignment);
             this.creep.moveTo(pos);
