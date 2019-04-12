9e0325fba8ccf20118510ceb951b7642a0070835
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 19:36:11 -0700

    log

--- KCODE3/scout.js
+++ KCODE3/scout.js
@@ -5,7 +5,7 @@ exports.scout = class{
         if(this.assignment){
             let pos = new RoomPosition(25,25,this.assignment);
             let path = this.findPath(pos);
-            console.log(path[0]);
+            console.log(path);
             console.log(this.creep.moveByPath(path));
         }
     }
