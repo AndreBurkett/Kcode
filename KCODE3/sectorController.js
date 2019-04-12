26aa47afeea79f43448dacb996c3fa67e3219642
AndreBurkett <andre.burkett@gmail.com>
2019-04-12 12:00:17 -0700

    show max

--- KCODE3/sectorController.js
+++ KCODE3/sectorController.js
@@ -138,6 +138,7 @@ exports.sectorController = class{
 
     gui(){
         let gui = new RoomVisual(this.room.name);
-        gui.text('Upgraders: ' + 0, 1,40, {align:'left'});
+        gui.text('Upgraders:', 1,40, {align:'left'});
+        gui.text(0 + '/1', 6,40, {align:'left'});
     }
 }
