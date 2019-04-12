ac3a7f500a0421b625a063c9beff866c13ba0118
AndreBurkett <andre.burkett@gmail.com>
2019-04-12 01:38:34 -0700

    debug

--- KCODE3/role.js
+++ KCODE3/role.js
@@ -25,7 +25,7 @@ exports.role = class{
 
     safePath(pos){
         let p = PathFinder.search(this.creep.pos, {pos:pos, range:1}, {
-            maxOps = 3000,
+            maxOps: 3000,
             roomCallback: function(roomName) {
                 if(Memory.sector[roomName]){
                     if(Memory.sector[roomName].CostMatrix){
