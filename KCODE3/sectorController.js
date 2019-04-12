375732e3aecf51619005de537e729eef1677a162
AndreBurkett <andre.burkett@gmail.com>
2019-04-12 12:31:07 -0700

    get rooms in range

--- KCODE3/sectorController.js
+++ KCODE3/sectorController.js
@@ -1,11 +1,16 @@
 exports.sectorController = class{
     constructor(name){
         this.name = name;
-        //this.getRoomsInRange();
+        this.rooms = this.getRoomsInRange();
     }
     getRoomsInRange(){
+        let rooms = [];
         for(let name of Object.keys(Memory.sector)){
-            
+            let d = Game.map.getRoomLinearDistance(this.name, name);
+            if(d <= 2){
+                rooms.push(d);
+            }
         }
+        return rooms;
     }
 }
