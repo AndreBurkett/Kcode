2cbfa1a1de0a3a74ddbcdad6a5bb6e6fce1dab4f
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 21:28:35 -0700

    extend role

--- KCODE3/scout.js
+++ KCODE3/scout.js
@@ -2,7 +2,8 @@ var role = require('role');
 
 exports.scout = class scout extends role.role{
     constructor(creep){
-        this.creep = creep;
+        super(creep);
+        //this.creep = creep;
         this.assignment = creep.memory.assignment;
         if(this.assignment){
             let pos = new RoomPosition(25,25,this.assignment);
