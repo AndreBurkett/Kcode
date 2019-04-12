8d7f98fb9b28faf3dee4077f9f4f6bd39f3c8910
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 22:52:41 -0700

    debug

--- KCODE3/role.js
+++ KCODE3/role.js
@@ -5,17 +5,27 @@ exports.role = class{
     }
 
     safeMove(pos){
-        if(!this.creep.memory.path){
-            let p = PathFinder.search(this.creep.pos, {pos:pos, range:1}, {
-                roomCallback: function(roomName) {
-                    if(Memory.sector[roomName].CostMatrix){
-                        let costs = PathFinder.CostMatrix.deserialize(Memory.sector[roomName].CostMatrix);
-                        return costs;
-                    }
-                }
-            });
-            this.creep.memory.path = p.path;
+        if(this.creep.memory.path){
+            let len = this.creep.memory.path.length;
+            let pos = new RoomPosition(this.creep.memory.path[len].x, this.creep.memory.path[len].y, this.creep.memory.path[len].roomName);
+            if(pos == creep.pos){
+                delete creep.memory.path;
+                this.safePath(pos);
+            }
         }
+        else this.safePath(pos);
         this.creep.moveByPath(this.creep.memory.path);
+    }
+
+    safePath(pos){
+        let p = PathFinder.search(this.creep.pos, {pos:pos, range:1}, {
+            roomCallback: function(roomName) {
+                if(Memory.sector[roomName].CostMatrix){
+                    let costs = PathFinder.CostMatrix.deserialize(Memory.sector[roomName].CostMatrix);
+                    return costs;
+                }
+            }
+        });
+        this.creep.memory.path = p.path;
     }
 }
