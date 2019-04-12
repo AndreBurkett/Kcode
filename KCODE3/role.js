f4932092e33544e696f6f08bbe3639718250c8eb
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 23:08:55 -0700

    log

--- KCODE3/role.js
+++ KCODE3/role.js
@@ -14,6 +14,7 @@ exports.role = class{
             }
         }
         else this.safePath(pos);
+        console.log(this.creep.pos.getDirectionTo(this.creep.memory.path[0]));
         console.log(this.creep.move(this.creep.pos.getDirectionTo(this.creep.memory.path[0])));
         switch(this.creep.move(this.creep.pos.getDirectionTo(this.creep.memory.path[0]))){
             case OK:
