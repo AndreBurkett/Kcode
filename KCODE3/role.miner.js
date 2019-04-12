6a2593b87e2da43f33908ed459ef90809bbe29fa
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 21:32:30 -0700

    extend role

--- KCODE3/role.miner.js
+++ KCODE3/role.miner.js
@@ -1,50 +1,52 @@
-var miner = {
-    run: function (creep) {
-        function transport(){
-            let target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (s) => (s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity) || ((s.structureType == STRUCTURE_EXTENSION || s.structureType == STRUCTURE_SPAWN) && s.energy < s.energyCapacity)
-            });
-            if (target){
-                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target);
-                }
-            }
-            else dump();
-        }
+var role = require('role');
 
-        function dump(){
-            let pos = new RoomPosition(Memory.source[source].spawnPath.path[0].x, Memory.source[source].spawnPath.path[0].y, Memory.source[source].spawnPath.path[0].roomName);
-            let container = pos.lookFor(LOOK_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER})[0];
-            if(container){
-                if(creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) creep.moveTo(container);
-            }
-            else{
-                let target = Game.getObjectById(source);
-                if(creep.harvest(target) == ERR_NOT_IN_RANGE) creep.moveTo(target);
-            }
-        }
+exports.miner = class miner extends role.role{
+    constructor(creep) {
+        super(creep);
         
-        let source = creep.memory.assignment;
+        let source = this.creep.memory.assignment;
         if(source){
-            if(creep.carry.energy == creep.carryCapacity){
+            if(this.creep.carry.energy == this.creep.carryCapacity){
                 if(Memory.source[source].transporter.length == 0){
-                    transport();
+                    this.transport();
                 }
                 else{
-                    dump();
+                    this.dump();
                 }
             }
             else{
                 let target = Game.getObjectById(source);
                 if(target){
-                    if(creep.harvest(target) == ERR_NOT_IN_RANGE) creep.moveTo(target);
+                    if(this.creep.harvest(target) == ERR_NOT_IN_RANGE) this.creep.moveTo(target);
                 }
                 else{
                     target = new RoomPosition(Memory.source[source].pos.x, Memory.source[source].pos.y, Memory.source[source].pos.roomName);
-                    creep.moveTo(target);
+                    this.creep.moveTo(target);
                 }
             }
         }
     }
+    transport(){
+        let target = this.creep.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (s) => (s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity) || ((s.structureType == STRUCTURE_EXTENSION || s.structureType == STRUCTURE_SPAWN) && s.energy < s.energyCapacity)
+        });
+        if (target){
+            if (this.creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                this.creep.moveTo(target);
+            }
+        }
+        else this.dump();
+    }
+
+    dump(){
+        let pos = new RoomPosition(Memory.source[source].spawnPath.path[0].x, Memory.source[source].spawnPath.path[0].y, Memory.source[source].spawnPath.path[0].roomName);
+        let container = pos.lookFor(LOOK_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER})[0];
+        if(container){
+            if(this.creep.transfer(container, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) this.creep.moveTo(container);
+        }
+        else{
+            let target = Game.getObjectById(source);
+            if(this.creep.harvest(target) == ERR_NOT_IN_RANGE) this.creep.moveTo(target);
+        }
+    }
 };
-module.exports = miner;
