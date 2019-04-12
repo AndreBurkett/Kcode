ba95f20bbdc3a29685d29f7a0db0b6316d8d637f
AndreBurkett <andre.burkett@gmail.com>
2019-04-12 12:26:23 -0700

    log

--- KCODE3/sectorController.js
+++ KCODE3/sectorController.js
@@ -1,6 +1,7 @@
 exports.sectorController = class{
     constructor(name){
         this.name = name;
+        this.getRoomsInRange();
     }
     getRoomsInRange(){
         for(let i of Memory.sector){
