9550c2896398e0c060916066b797cc56ecf34b50
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 19:22:08 -0700

    avoid hostile rooms

--- KCODE3/scout.js
+++ KCODE3/scout.js
@@ -4,7 +4,23 @@ exports.scout = class{
         this.assignment = creep.memory.assignment;
         if(this.assignment){
             let pos = new RoomPosition(25,25,this.assignment);
-            this.creep.moveTo(pos);
+            this.creep.moveByPath(pos);
         }
+    }
+    findPath(pos){
+        let path = PathFinder.search(this.creep.pos, pos, {
+            roomCallback: function(roomName) {
+                if(Memory.sector[roomName] && Memory.sector[roomName].owner == 'hostile'){
+                    let costs = new PathFinder.CostMatrix;
+                    for(let x = 0; x<50; x++){
+                        for(let y = 0; y<50; y++){
+                            costs.set(x, y, 255);
+                        }
+                    }
+                }
+                return costs;
+            }
+        });
+        return path;
     }
 }
