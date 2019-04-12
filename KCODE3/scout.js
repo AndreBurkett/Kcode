ff97a9bb418cfbd011db7c30b193dcd990562eaf
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 20:34:26 -0700

    refactor

--- KCODE3/scout.js
+++ KCODE3/scout.js
@@ -18,28 +18,10 @@ exports.scout = class{
     findPath(pos){
         let path = PathFinder.search(this.creep.pos, {pos:pos, range:10}, {
             roomCallback: function(roomName) {
-                if(Memory.sector[roomName]){
-                    if(Memory.sector[roomName].owner == 'hostile'){
-                        let costs;
-                        if(!Memory.sector[roomName].CostMatrix){
-                            costs = new PathFinder.CostMatrix;
-                            for(let x = 0; x<50; x++){
-                                for(let y = 0; y<50; y++){
-                                    costs.set(x, y, 255);
-                                }
-                            }
-                            Memory.sector[roomName].CostMatrix = costs.serialize();
-                        }
-                        else{
-                            costs = PathFinder.CostMatrix.deserialize(Memory.sector[roomName].CostMatrix);
-                        }
-                        return costs;
-                    }
-                    else if(Memory.sector[roomName].owner){
-
-                    }
+                if(Memory.sector[roomName].CostMatrix){
+                    costs = PathFinder.CostMatrix.deserialize(Memory.sector[roomName].CostMatrix);
+                    return costs;
                 }
-                //return;
             }
         });
         return path;
