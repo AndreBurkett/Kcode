2b4a6e6a454f4695c47ede8e9301b9a2d63e735a
AndreBurkett <andre.burkett@gmail.com>
2019-04-11 23:16:13 -0700

    debug

--- KCODE3/role.js
+++ KCODE3/role.js
@@ -14,8 +14,7 @@ exports.role = class{
             }
         }
         else this.safePath(pos);
-        let mpos = new RoomPosition(this.creep.memory.path[0]);
-        console.log(mpos);
+        let mpos = new RoomPosition(this.creep.memory.path[0].x, this.creep.memory.path[0].y, this.creep.memory.path[0].roomName);
         console.log(this.creep.pos.getDirectionTo(this.creep.memory.path[0]));
         console.log(this.creep.move(this.creep.pos.getDirectionTo(this.creep.memory.path[0])));
         switch(this.creep.move(this.creep.pos.getDirectionTo(this.creep.memory.path[0]))){
