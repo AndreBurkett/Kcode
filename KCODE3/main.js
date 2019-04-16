d47b8274282809147928da12c7f8f8e038fd8b2c
AndreBurkett <andre.burkett@gmail.com>
2019-04-16 01:09:57 -0700

    log

--- KCODE3/main.js
+++ KCODE3/main.js
@@ -47,7 +47,7 @@ module.exports.loop = function(){
         Memory.stats.cpu.move          = Memory.moveCpu;
 
         //Screeps stats
-        console.log(Game.creeps);
+        console.log(Object.keys(Game.creeps));
         Memory.stats.creeps.count      = Game.creeps;
       }
 }
