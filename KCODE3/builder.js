294d576f24185e8971d091d6ace1a881c008d48a
AndreBurkett <andre.burkett@gmail.com>
2019-04-19 14:52:43 -0700

    debug

--- KCODE3/builder.js
+++ KCODE3/builder.js
@@ -15,19 +15,19 @@ exports.builder = class builder extends role.role{
                 delete this.creep.memory.assignment;
                 target = this.creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.hits < s.hitsMax});
                 if(target){
-                    if(this.creep.repair(target) == ERR_NOT_IN_RANGE) this.creep.moveTo(target);
+                    if(this.creep.repair(target) == ERR_NOT_IN_RANGE) this.safeMove(target.pos);
                 }
             }
         }
         else if(this.creep.room.find(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 0}).length > 0){
             this.creep.memory.task = 'pickup';
             target = this.creep.pos.findClosestByRange(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > this.creep.carryCapacity});
-            if(this.creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) this.creep.moveTo(target);
+            if(this.creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) this.safeMove(target);
         }
         else{
             this.creep.memory.task = 'pickup';
             target = this.creep.pos.findClosestByRange(FIND_DROPPED_RESOURCES, {filter: (r) => r.resourceType == RESOURCE_ENERGY});
-            if (this.creep.pickup(target) == ERR_NOT_IN_RANGE) this.creep.moveTo(target);
+            if (this.creep.pickup(target) == ERR_NOT_IN_RANGE) this.safeMove(target);
         }
     }
 }
