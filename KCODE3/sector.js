b8a4749a70234a52ec4ae4ef6a594929f319a002
AndreBurkett <andre.burkett@gmail.com>
2019-04-19 16:25:36 -0700

    debug

--- KCODE3/sector.js
+++ KCODE3/sector.js
@@ -12,6 +12,7 @@ exports.sector = class{
         if(!Memory.sector[name].source) Memory.sector[name].source = {};
         if(!Memory.sector[name].spawn) Memory.sector[name].spawn = {};
         if(!Memory.sector[name].storage) Memory.sector[name].storage = {};
+        if(!Memory.sector[name].storage.keeper) Memory.sector[name].storage.keeper = [];
         if(!Memory.sector[name].subSector) Memory.sector[name].subSector = {};
 
         //Create Roles
