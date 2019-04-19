3aa0f1fc6320f031ab0cd92dda7a9c5eb61c4e92
AndreBurkett <andre.burkett@gmail.com>
2019-04-19 14:47:23 -0700

    log

---
+++ KCODE3/keeper.js
@@ -0,0 +1,11 @@
+var role = require('role');
+
+exports.keeper = class keeper extends role.role{
+    constructor(creep) {
+        super(creep);
+        
+        if(this.assignment){
+            
+        }
+    }
+}
