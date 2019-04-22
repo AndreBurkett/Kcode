529668905ac30e4eb92fcd11ffdd9470dd6b8585
AndreBurkett <andre.burkett@gmail.com>
2019-04-22 09:52:24 -0700

    debug

--- KCODE3/bunkerController.js
+++ KCODE3/bunkerController.js
@@ -13,14 +13,14 @@ exports.bunkerController = class{
     build(site){
         let pos = new RoomPosition(this.x + site.x, this.y + site.y, this.room.name);
         let constructionExists = pos.lookFor(LOOK_CONSTRUCTION_SITES, {filter: (s) => s.structureType == site.s})[0];
-        if(constructionExists == false){
-            let siteExists = pos.lookFor(LOOK_STRUCTURES, {filter: (s) => s.structureType == site.s});
+        if(constructionExists) return true
+        else{
+            let siteExists = pos.lookFor(LOOK_STRUCTURES, {filter: (s) => s.structureType == site.s})[0];
             console.log('s', pos, siteExists);
             if(siteExists) return false;
             this.room.createConstructionSite(pos, site.s);
             return true;
         }
-        return true;
     }
 
     createRoads(max){
