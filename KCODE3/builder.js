d0c09895d572a1ab99aa31710c7106b504293b7e
AndreBurkett <andre.burkett@gmail.com>
2019-04-21 23:30:27 -0700

    debug

--- KCODE3/builder.js
+++ KCODE3/builder.js
@@ -22,7 +22,7 @@ exports.builder = class builder extends role.role{
         else{
             this.creep.memory.task = 'pickup';
             target = this.creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                filter: (s) => s.structureType == (STRUCTURE_CONTAINER || STRUCTURE_STORAGE) && s.store[RESOURCE_ENERGY] > this.creep.carryCapacity});
+                filter: (s) => (s.structureType == STRUCTURE_CONTAINER || s.structureType == STRUCTURE_STORAGE) && s.store[RESOURCE_ENERGY] > this.creep.carryCapacity});
             if(target){
                 if(this.creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) this.safeMove(target.pos);
             }
